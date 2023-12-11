6f9b02a0710def4e4030e1d944639d56a6c9e87f
Matthieu <matthieu.fanget1@gmail.com>
2023-12-11 15:01:43 +0100

    Ajout de page html ainsi que d'une connection à la bdd

---
+++ index.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'mysql-matthieu-73.alwaysdata.net', 
+    user: '339794_aymeric',
+    password: '339794_aymeric_projet_dev_auth',
+    database: 'matthieu-73_projet_dev_auth'
+});
+
+connection.connect((err) => {
+    if(err) throw err;
+    console.log('Connected!');
+}
+);
+
+module.exports = connection;
