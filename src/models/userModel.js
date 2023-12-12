fb7e1e0b707526714573ce5b23c88d678acd41c3
Matthieu <matthieu.fanget1@gmail.com>
2023-12-12 14:12:40 +0100

    Correction bug

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,9 +1,10 @@
 const connection = require('../db');
 
 exports.createUser = (username, hashedPassword) => {
+    max_user_id = this.getMaxIdUser();
     return new Promise((resolve, reject) => {
-        const sql = 'INSERT INTO users (mail, password) VALUES (?, ?)';
-        connection.query(sql, [username, hashedPassword], (err, result) => {
+        const sql = 'INSERT INTO users (user_id,mail, password) VALUES (?, ?, ?)';
+        connection.query(sql, [max_user_id, username, hashedPassword], (err, result) => {
             if (err) {
                 reject(err);
             } else {
@@ -12,5 +13,23 @@ exports.createUser = (username, hashedPassword) => {
         });
     });
 };
+
+exports.getMaxIdUser = () => {
+    let user_id = 0;  
+
+    // Récupération du maximum user_id
+    const sql_max_user_id = 'SELECT max(user_id) AS max_user_id from users';
+    const maxUserIdResult = connection.query(sql_max_user_id, (err, result) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve(result);
+        }
+    });
+
+    user_id = maxUserIdResult[0].max_user_id + 1;
+
+    return user_id;
+}
 
 // Ajoutez ici d'autres fonctions pour interagir avec la table des utilisateurs
